5e4a4d0336dfa332aca108f5282d9a0f1cfebbc3
tahiryK <110636209+tahiryK@users.noreply.github.com>
2022-12-22 15:32:20 +0300

    Delete fanorona directory

--- fanorona/Client/connected.bat
+++
@@ -1,8 +0,0 @@
-javac echangeVersServer/*.java 
-javac joueurClient/*.java 
-javac socketClient/*.java 
-javac sourisClient/*.java
-javac terrainClient/*.java
-javac main/*.java
-java main/Main
-
--- fanorona/Server/connected.bat
+++
@@ -1,7 +0,0 @@
-javac echangeVersClient/*.java 
-javac joueurServer/*.java 
-javac socketServer/*.java 
-javac sourisServer/*.java
-javac terrainServer/*.java
-javac main/*.java
-java main/Main
